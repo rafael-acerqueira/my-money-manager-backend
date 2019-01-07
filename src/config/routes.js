f813fa360645509e18ee1bee738a28b0377b032c
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-07 10:50:02 -0300

    Adjust route name

--- src/config/routes.js
+++ src/config/routes.js
@@ -6,5 +6,5 @@ module.exports = function(server) {
   server.use('/api', router)
 
   const BillingCycle = require('../api/billingCycle/billingCycleService')
-  BillingCycle.register(router, '/billingCyles')
+  BillingCycle.register(router, '/billingCycles')
 }

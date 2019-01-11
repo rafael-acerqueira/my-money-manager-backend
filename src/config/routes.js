b78994e8b74ad2cd5414bdacbaba7aca641fb898
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-11 12:28:53 -0300

    Adjust name of service

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
 
-  const router = express.Router()
-  server.use('/api', router)
+  const protectedApi = express.Router()
+  server.use('/api', protectedApi)
+  
+  protectedApi.use(auth)
 
   const BillingCycle = require('../api/billingCycle/billingCycleService')
-  BillingCycle.register(router, '/billingCycles')
+  BillingCycle.register(protectedApi, '/billingCycles')
+
+
+  const openApi = express.Router()
+  server.use('/api', openApi)
+
+  const AuthService = require('../api/user/authService')
+  openApi.post('/login', AuthService.login)
+  openApi.post('/signup', AuthService.signup)
+  openApi.post('/validateToken', AuthService.validateToken)
 }

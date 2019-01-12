1fbdd8b70feab018b4a110e3a1143afef8aef053
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-12 17:24:54 -0300

    Upate database url

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 
+const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : 
             'mongodb://localhost/mymoneymanager'
 
 module.exports = mongoose.connect(url, { useNewUrlParser: true })
